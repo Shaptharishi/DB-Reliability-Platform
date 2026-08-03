d446bc4a347d367800dd255f5eb7d9d5fe43e3d6
Shaptharishi <rishiaustin1947@gmail.com>
2026-08-04 00:41:18 +0530

    Remove local DB containers, restore ClickHouse memory limit config

--- terraform-dbrp/ec2.tf
+++ terraform-dbrp/ec2.tf
@@ -10,10 +10,21 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "collector" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = "db-reliability-ec2-key"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   subnet_id              = data.aws_subnets.default.ids[0]
+
+  user_data_replace_on_change = true
+
+  user_data = templatefile("${path.module}/user_data.sh.tpl", {
+    postgres_host      = aws_db_instance.postgres.address
+    postgres_password  = var.postgres_password
+    mysql_host         = aws_db_instance.mysql.address
+    mysql_password     = var.mysql_password
+    redis_host         = aws_elasticache_replication_group.redis.primary_endpoint_address
+    slack_webhook_url  = var.slack_webhook_url
+  })
 
   tags = {
     Name = "db-reliability-collector-tf"
